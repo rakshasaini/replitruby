9f45b5a825ffceed85dc0d2717b5e814ee9f0fa0
Raksha Saini <rakshasaini20@gmail.com>
2022-01-31 23:40:30 +0000

    bcrypt the password

--- password.rb
+++ password.rb
@@ -14,12 +14,16 @@ end
 require 'bcrypt'
 
 my_password = BCrypt::Password.create("my password")
-#=> "$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey"
+# every time the pasword will change when it runs see below:
+#=> $2a$12$UmLPVlUfdvr/oyt3hM9xcOFxwSjSjIpb0HBYOSJgNz9AF.ZO2PiWi
+#=> $2a$12$jP3hTDibMZF6caQSSSjUvOl7irfumAxDjyhbnHLKKcEFthdLylH3W
+#=> $2a$12$5FM/qIBPU37cxYyYiVG0fu.zLMvL/XmVlH8XygP.A/X7Qe9i9fcEe
 
+puts my_password
 puts my_password.version              #=> "2a"
 puts my_password.cost                 #=> 12
-my_password == "my password"     #=> true
-my_password == "not my password" #=> false
+puts my_password == "my password"     #=> true
+puts my_password == "not my password" #=> false
 
 # my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
 # my_password == "my password"     #=> true

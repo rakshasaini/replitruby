952917d072ff2f479e0a6c99e9e22f2e09841f61
Raksha Saini <rakshasaini20@gmail.com>
2022-01-31 23:49:16 +0000

    compare the password and get bcrypt password compare with enter text password.

--- password.rb
+++ password.rb
@@ -13,18 +13,22 @@ end
 
 require 'bcrypt'
 
-my_password = BCrypt::Password.create("my password")
+# my_password = BCrypt::Password.create("my password")
 # every time the pasword will change when it runs see below:
 #=> $2a$12$UmLPVlUfdvr/oyt3hM9xcOFxwSjSjIpb0HBYOSJgNz9AF.ZO2PiWi
 #=> $2a$12$jP3hTDibMZF6caQSSSjUvOl7irfumAxDjyhbnHLKKcEFthdLylH3W
 #=> $2a$12$5FM/qIBPU37cxYyYiVG0fu.zLMvL/XmVlH8XygP.A/X7Qe9i9fcEe
 
-puts my_password
+# puts my_password
+# puts my_password.version              #=> "2a"
+# puts my_password.cost                 #=> 12
+# puts my_password == "my password"     #=> true
+# puts my_password == "not my password" #=> false
+
+
+#Before this one time we check the store value in my_password and copy and past the password in my .new (password) ..this work on last exceuation or run. the salt funcation is change the password everytime.
+puts my_password = BCrypt::Password.new("$2a$12$dVLLKwpY4VsBtfuxjsWqIeL7HrWE480Q73avn.fsFVjg0j1u4yOGm")
 puts my_password.version              #=> "2a"
 puts my_password.cost                 #=> 12
 puts my_password == "my password"     #=> true
 puts my_password == "not my password" #=> false
-
-# my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
-# my_password == "my password"     #=> true
-# my_password == "not my password" #=> false

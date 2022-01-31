ff9eff341db7ad78cd51ed7b0820389c9f7272d4
Raksha Saini <rakshasaini20@gmail.com>
2022-01-31 23:31:31 +0000

    How to add gem in replit

---
+++ password.rb
@@ -0,0 +1,26 @@
+#Its a importan ruby gem bcrypt link
+# https://www.rubydoc.info/github/codahale/bcrypt-ruby/
+# https://github.com/bcrypt-ruby/bcrypt-ruby/tree/master
+# to instll gem use :- 
+# gem install bcrypt
+
+require 'bundler/inline'
+
+gemfile true do
+  source 'http://rubygems.org'
+  gem 'bcrypt'
+end
+
+require 'bcrypt'
+
+my_password = BCrypt::Password.create("my password")
+#=> "$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey"
+
+puts my_password.version              #=> "2a"
+puts my_password.cost                 #=> 12
+my_password == "my password"     #=> true
+my_password == "not my password" #=> false
+
+# my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
+# my_password == "my password"     #=> true
+# my_password == "not my password" #=> false

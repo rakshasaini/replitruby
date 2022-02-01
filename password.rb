773a6eb9fbd1726c8a6a8a6146c718e0a8f9921d
Raksha Saini <rakshasaini20@gmail.com>
2022-02-01 00:00:26 +0000

    see how bcrypt works

--- password.rb
+++ password.rb
@@ -19,6 +19,16 @@ require 'bcrypt'
 #=> $2a$12$jP3hTDibMZF6caQSSSjUvOl7irfumAxDjyhbnHLKKcEFthdLylH3W
 #=> $2a$12$5FM/qIBPU37cxYyYiVG0fu.zLMvL/XmVlH8XygP.A/X7Qe9i9fcEe
 
+#see how password have different value each time
+my_password = BCrypt::Password.create("my password")
+my_password1 = BCrypt::Password.create("my password")
+my_password2 = BCrypt::Password.create("my password")
+
+#here we see that the value of my password change each time when it execute.
+puts my_password
+puts my_password1
+puts my_password2
+
 # puts my_password
 # puts my_password.version              #=> "2a"
 # puts my_password.cost                 #=> 12
@@ -27,8 +37,8 @@ require 'bcrypt'
 
 
 #Before this one time we check the store value in my_password and copy and past the password in my .new (password) ..this work on last exceuation or run. the salt funcation is change the password everytime.
-puts my_password = BCrypt::Password.new("$2a$12$dVLLKwpY4VsBtfuxjsWqIeL7HrWE480Q73avn.fsFVjg0j1u4yOGm")
-puts my_password.version              #=> "2a"
-puts my_password.cost                 #=> 12
-puts my_password == "my password"     #=> true
-puts my_password == "not my password" #=> false
+# puts my_password = BCrypt::Password.new("$2a$12$dVLLKwpY4VsBtfuxjsWqIeL7HrWE480Q73avn.fsFVjg0j1u4yOGm")
+# puts my_password.version              #=> "2a"
+# puts my_password.cost                 #=> 12
+# puts my_password == "my password"     #=> true
+# puts my_password == "not my password" #=> false

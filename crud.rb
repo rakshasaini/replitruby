72d23d1042a9a4d247cf8006391ee73cbd464a97
Raksha Saini <rakshasaini20@gmail.com>
2022-02-01 01:43:37 +0000

    multiple checks methods

--- crud.rb
+++ crud.rb
@@ -22,13 +22,34 @@ def create_hash_verify(password)
   BCrypt::Password.new(password)
 end
 
-def create_secure_users(list_of_users)
-  list_of_users.each do |user_records|
-    puts user_records[:password]
+# def create_secure_users(list_of_users)
+#   list_of_users.each do |user_records|
+#     puts user_records[:password]
+#   end
+# end
+# create_secure_users(users)
+# puts "done password"
+
+
+#get the list of username
+def create_users_details(users_details)
+  puts "Now User Names:"
+  users_details.each do |user_names|
+    puts user_names[:username]
+  end
+  puts "Now Passwords"
+  users_details.each do |user_pass|
+    puts user_pass[:password]
   end
 end
+puts "The all users are"
+create_users_details(users)
 
-create_secure_users(users)
+#The value in create_username_users and create_secure_users are same check this:
+# puts create_secure_users(users)
+# puts create_username_users(users)
+
+
 
 
 
